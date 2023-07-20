b9f0326083a398510cac99fbcec2d82e7a4005f1
Philippa Markovics <philippa@markovics.com>
2023-07-20 14:35:04 +0200

    Also provide command-bar ns to sci ctx

--- demo/src/nextjournal/command_bar/demo.cljs
+++ demo/src/nextjournal/command_bar/demo.cljs
@@ -4,6 +4,7 @@
             ["react-dom/client" :as react-client]
             [nextjournal.command-bar.demo.sci :as demo.sci]
             [nextjournal.command-bar.demo.editor :as editor]
+            [nextjournal.command-bar :as command-bar]
             [reagent.core :as r]
             [sci.core :as sci]
             [sci.ctx-store]))
@@ -15,4 +16,4 @@
   (when react-root
     (.render react-root (r/as-element [editor/root]))))
 
-(sci.ctx-store/swap-ctx! sci/merge-opts {:namespaces (sci-copy-nss 'nextjournal.command-bar.demo.editor)})
+(sci.ctx-store/swap-ctx! sci/merge-opts {:namespaces (sci-copy-nss 'nextjournal.command-bar.demo.editor 'nextjournal.command-bar)})
